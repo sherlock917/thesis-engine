374e393a225ff32bdeba7cccc897a836346c2d1e
SherlockZhong <sherlock.z0917@gmail.com>
2015-06-25 17:23:33 +0800

    changed links

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var request = require('request')
 
 mongoose.connect('mongodb://localhost/ThesisEngine')
 
-var urlSession = 'http://epub.cnki.net/KNS/request/SearchHandler.ashx?action=&NaviCode=*&ua=1.21&PageName=ASP.brief_result_aspx&DbPrefix=CDFD&DbCatalog=%e4%b8%ad%e5%9b%bd%e5%8d%9a%e5%a3%ab%e5%ad%a6%e4%bd%8d%e8%ae%ba%e6%96%87%e5%85%a8%e6%96%87%e6%95%b0%e6%8d%ae%e5%ba%93&ConfigFile=CDFD.xml&db_opt=%E4%B8%AD%E5%9B%BD%E5%8D%9A%E5%A3%AB%E5%AD%A6%E4%BD%8D%E8%AE%BA%E6%96%87%E5%85%A8%E6%96%87%E6%95%B0%E6%8D%AE%E5%BA%93&db_value=%E4%B8%AD%E5%9B%BD%E5%8D%9A%E5%A3%AB%E5%AD%A6%E4%BD%8D%E8%AE%BA%E6%96%87%E5%85%A8%E6%96%87%E6%95%B0%E6%8D%AE%E5%BA%93&his=0'
-  , urlMain = 'http://epub.cnki.net/KNS/brief/brief.aspx?RecordsPerPage=50&QueryID=2&ID=&turnpage=1&tpagemode=L&dbPrefix=CDFD&Fields=&DisplayMode=custommode&PageName=ASP.brief_result_aspx&curpage='
+var urlSession = 'http://epub.cnki.net/KNS/request/SearchHandler.ashx?action=&NaviCode=*&ua=1.21&PageName=ASP.brief_result_aspx&DbPrefix=CMFD&DbCatalog=%e4%b8%ad%e5%9b%bd%e4%bc%98%e7%a7%80%e7%a1%95%e5%a3%ab%e5%ad%a6%e4%bd%8d%e8%ae%ba%e6%96%87%e5%85%a8%e6%96%87%e6%95%b0%e6%8d%ae%e5%ba%93&ConfigFile=CMFD.xml&db_opt=%E4%B8%AD%E5%9B%BD%E4%BC%98%E7%A7%80%E7%A1%95%E5%A3%AB%E5%AD%A6%E4%BD%8D%E8%AE%BA%E6%96%87%E5%85%A8%E6%96%87%E6%95%B0%E6%8D%AE%E5%BA%93&db_value=%E4%B8%AD%E5%9B%BD%E4%BC%98%E7%A7%80%E7%A1%95%E5%A3%AB%E5%AD%A6%E4%BD%8D%E8%AE%BA%E6%96%87%E5%85%A8%E6%96%87%E6%95%B0%E6%8D%AE%E5%BA%93&his=0'
+  , urlMain = 'http://epub.cnki.net/KNS/brief/brief.aspx?RecordsPerPage=50&QueryID=2&ID=&turnpage=1&tpagemode=L&dbPrefix=CMFD&Fields=&DisplayMode=custommode&PageName=ASP.brief_result_aspx&curpage='
 
 var session
 var page = 1, count = 0
@@ -81,8 +81,9 @@ function decode (body) {
 }
 
 function save (data) {
-  for (var i = 0; i < data.length; i++) {
-    var brief = new Brief(data[i])
-    brief.save()
-  }
+  console.log(data)
+  // for (var i = 0; i < data.length; i++) {
+  //   var brief = new Brief(data[i])
+  //   brief.save()
+  // }
 }
