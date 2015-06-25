25a7fa3c64e53c67c53d8846dc6beea0dffd41a8
SherlockZhong <sherlock.z0917@gmail.com>
2015-06-25 17:14:38 +0800

    changed link

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var request = require('request')
 
 mongoose.connect('mongodb://localhost/ThesisEngine')
 
-var urlSession = 'http://epub.cnki.net/KNS/request/SearchHandler.ashx?action=&NaviCode=*&ua=1.21&PageName=ASP.brief_result_aspx&DbPrefix=CJFQ&DbCatalog=%e4%b8%ad%e5%9b%bd%e5%ad%a6%e6%9c%af%e6%9c%9f%e5%88%8a%e7%bd%91%e7%bb%9c%e5%87%ba%e7%89%88%e6%80%bb%e5%ba%93&ConfigFile=CJFQ.xml&db_opt=%E4%B8%AD%E5%9B%BD%E5%AD%A6%E6%9C%AF%E6%9C%9F%E5%88%8A%E7%BD%91%E7%BB%9C%E5%87%BA%E7%89%88%E6%80%BB%E5%BA%93&db_value=%E4%B8%AD%E5%9B%BD%E5%AD%A6%E6%9C%AF%E6%9C%9F%E5%88%8A%E7%BD%91%E7%BB%9C%E5%87%BA%E7%89%88%E6%80%BB%E5%BA%93&year_type=echar'
-  , urlMain = 'http://epub.cnki.net/KNS/brief/brief.aspx?RecordsPerPage=50&QueryID=0&ID=&turnpage=1&tpagemode=L&dbPrefix=CJFQ&Fields=&DisplayMode=custommode&PageName=ASP.brief_result_aspx&curpage='
+var urlSession = 'http://epub.cnki.net/KNS/request/SearchHandler.ashx?action=&NaviCode=*&ua=1.21&PageName=ASP.brief_result_aspx&DbPrefix=CDFD&DbCatalog=%e4%b8%ad%e5%9b%bd%e5%8d%9a%e5%a3%ab%e5%ad%a6%e4%bd%8d%e8%ae%ba%e6%96%87%e5%85%a8%e6%96%87%e6%95%b0%e6%8d%ae%e5%ba%93&ConfigFile=CDFD.xml&db_opt=%E4%B8%AD%E5%9B%BD%E5%8D%9A%E5%A3%AB%E5%AD%A6%E4%BD%8D%E8%AE%BA%E6%96%87%E5%85%A8%E6%96%87%E6%95%B0%E6%8D%AE%E5%BA%93&db_value=%E4%B8%AD%E5%9B%BD%E5%8D%9A%E5%A3%AB%E5%AD%A6%E4%BD%8D%E8%AE%BA%E6%96%87%E5%85%A8%E6%96%87%E6%95%B0%E6%8D%AE%E5%BA%93&his=0'
+  , urlMain = 'http://epub.cnki.net/KNS/brief/brief.aspx?RecordsPerPage=50&QueryID=2&ID=&turnpage=1&tpagemode=L&dbPrefix=CDFD&Fields=&DisplayMode=custommode&PageName=ASP.brief_result_aspx&curpage='
 
 var session
 var page = 1, count = 0
@@ -57,14 +57,14 @@ function decode (body) {
 
     // get title
     var text = $(titles[i]).text()
-    data.title = text.substring(59, text.length - 6)
+    data.title = text.substring(41, text.length - 5)
 
     // get authors and journals
     var green = $(contents[i]).find('.fontgreen').text().replace(/\s/g, '').split('，')
     data.author = green[0]
     data.institute = green[1]
-    data.journal = green[2]
-    data.issue = green[3]
+    // data.journal = green[2]
+    data.issue = green[2]
 
     // get abstract
     var script = $($(contents[i]).find('script')[0]).text()
