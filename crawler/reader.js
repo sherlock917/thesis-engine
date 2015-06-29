c1908b66add78bf362ca84dd59f03e93c68c9524
SherlockZhong <sherlock.z0917@gmail.com>
2015-06-29 20:24:32 +0800

    fixed

--- crawler/reader.js
+++ crawler/reader.js
@@ -1,8 +1,20 @@
-var mongoose = require('mongoose')
+var fs = require('fs')
+  , mongoose = require('mongoose')
   , Brief = require('./models/Brief')
 
 mongoose.connect('mongodb://localhost/ThesisEngine')
 
+var count = 0
+
 Brief.find(function (err, data) {
-  console.log(data.length)
+  console.log((new Date()) + ' -- start')
+  for (var i = 0; i < data.length; i++) {
+    fs.appendFile('result.txt', JSON.stringify(data[i]) + '\r\n', function (err) {
+      count++
+      if (count >= data.length) {
+        console.log((new Date()) + ' -- done')
+        mongoose.disconnect()
+      }
+    })
+  }
 })
